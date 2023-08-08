b84cf8e6398bba191716efe5326566fa947cedb9
natkaa <131975403+NataKanyuk@users.noreply.github.com>
2023-08-08 12:41:51 +0200

    Update script.js

--- lesson_23/script.js
+++ lesson_23/script.js
@@ -1,9 +1,9 @@
-let array = [12, 25, 6, 48, 33, 17, 9, 56, 40, 22];
+let array = [12, 25, 6, null, 48, 33, 17, 9, false, 56, 40, 22];
 console.log(array);
 
 let totalSum = 0;
 for (let i = 0; i < array.length; i++) {
-    if (typeof array[i] === 'number' || !isNaN(array[i])) {
+    if (typeof array[i] === 'number' && !isNaN(array[i])) {
         totalSum += array[i];
     }
 }
