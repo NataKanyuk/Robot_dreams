c710388dd73b666b77c62b9ae0027452cc84c37e
NataKanyuk <natakanyuk2@gmail.com>
2023-08-31 17:06:33 +0200

    lesson_31

---
+++ lesson_31/script.js
@@ -0,0 +1,22 @@
+const targetDate = new Date("2024-01-01 00:00:00").getTime();
+
+function updateCountdown() {
+    const currentDate = new Date().getTime();
+    const timeLeft = targetDate - currentDate;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = formatTime(days);
+    document.getElementById("hours").textContent = formatTime(hours);
+    document.getElementById("minutes").textContent = formatTime(minutes);
+    document.getElementById("seconds").textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updateCountdown, 1000);
